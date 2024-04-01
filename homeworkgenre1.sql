6338636b6b902d4bb08789333be7d2fd50132184
dermorgenmuffel <ihoujinihoujin3@gmail.com>
2024-04-01 10:51:01 +0700

    changed commits

--- homeworkgenre1.sql
+++ homeworkgenre1.sql
@@ -1,24 +1,11 @@
 CREATE TABLE IF NOT EXISTS musical_genre (
 	id SERIAL PRIMARY KEY, 
-	name VARCHAR(60) NOT NULL,
-	name INTEGER UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS genre_artist (
-	PRIMARY KEY(genre_id, artist_id),
-	genre_id INTEGER NOT NULL REFERENCES artist_list(id),
-	artist_id INTEGER NOT NULL REFERENCES musical_genre(id)
+	name VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS artist_list (
 	id SERIAL PRIMARY KEY, 
-	name_artist VARCHAR(60) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS artists_albums (
-	PRIMARY key(albums_id, artist_id), 
-	albums_id INTEGER NOT NULL REFERENCES albums(id),
-	artist_id INTEGER NOT NULL REFERENCES artist_list(id)
+	name_artist VARCHAR(60) NOT null
 );
 
 CREATE TABLE IF NOT EXISTS albums (
@@ -34,14 +21,27 @@ CREATE TABLE IF NOT EXISTS tracks_list (
 	album_id INTEGER REFERENCES albums(id)
 );
 
-CREATE TABLE IF NOT EXISTS tracks_collected (
-	PRIMARY key(track_id, collected_id),
-	track_id INTEGER NOT NULL REFERENCES tracks_list(id),
-	collected_id INTEGER NOT NULL REFERENCES collected(id)
-);
-
 CREATE TABLE IF NOT EXISTS collected (
 	id SERIAL PRIMARY KEY, 
 	collected_name VARCHAR(60) NOT NULL,
 	release DATE
+);
+
+CREATE TABLE IF NOT EXISTS genre_artist (
+	PRIMARY KEY(genre_id, artist_id),
+	genre_id INTEGER NOT NULL REFERENCES artist_list(id),
+	artist_id INTEGER NOT NULL REFERENCES musical_genre(id)
+);
+
+CREATE TABLE IF NOT EXISTS artists_albums (
+	PRIMARY key(albums_id, artist_id), 
+	albums_id INTEGER NOT NULL REFERENCES albums(id),
+	artist_id INTEGER NOT NULL REFERENCES artist_list(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS tracks_collected (
+	PRIMARY key(track_id, collected_id),
+	track_id INTEGER NOT NULL REFERENCES tracks_list(id),
+	collected_id INTEGER NOT NULL REFERENCES collected(id)
 );
--- select.sql
+++ select.sql
@@ -4,7 +4,7 @@ WHERE lasting = (SELECT MAX(lasting) FROM tracks_list);
 
 SELECT track_name
 FROM tracks_list
-WHERE lasting >= '00:03:50';
+WHERE lasting >= '00:03:30'
  
 SELECT collected_name
 FROM collected
@@ -16,7 +16,10 @@ WHERE name_artist NOT LIKE '% %';
 
 SELECT track_name
 FROM tracks_list
-WHERE (track_name LIKE '%мой%') OR (track_name LIKE '%my%');
+WHERE (track_name ILIKE 'my%')  
+OR (track_name ILIKE '% my%')  
+OR (track_name ILIKE '% my')   
+OR (track_name = 'my');        
 
 -- 3 задание
 SELECT artist_id, COUNT(DISTINCT genre_id) AS num_artists
@@ -42,17 +45,20 @@ FROM albums a
 JOIN tracks_list t ON a.id = t.album_id
 GROUP BY a.name_album;
 
-SELECT a.name_artist, al.name_album, al.release
-FROM artist_list a
-JOIN artists_albums aa ON a.id = aa.artist_id
-JOIN albums al ON aa.albums_id = al.id
-WHERE EXTRACT(YEAR FROM release) = 2020;
-
-SELECT collected_name
-FROM collected
-WHERE release IN (
-    SELECT release
-    FROM collected
-    JOIN artist_list ON collected.id = artist_list.id
-    WHERE name_artist = 'Lauri Ylönen'
+SELECT name_artist
+FROM artist_list
+WHERE id NOT IN (
+  SELECT artist_id
+  FROM artists_albums aa
+  JOIN albums al ON aa.albums_id = al.id
+  WHERE EXTRACT(YEAR FROM al.release) = 2020
 );
+
+SELECT DISTINCT collected_name
+FROM collected c
+JOIN tracks_collected tic ON c.id = tic.collected_id
+JOIN tracks_list t ON tic.track_id = t.id
+JOIN albums a ON t.album_id = a.id
+JOIN artists_albums aa ON a.id = aa.albums_id
+JOIN artist_list al ON aa.artist_id = al.id
+WHERE al.name_artist = 'Горшок';
